b9b23483c007fefcdd3fd4e27b49bdafe7938666
it-dreams <rahulraj.holi888@gmail.com>
2021-03-19 01:23:05 +0530

    add page not found component

---
+++ src/app/layout/page-404/page-404.component.ts
@@ -0,0 +1,14 @@
+import { Component } from "@angular/core";
+
+@Component({
+      selector: 'Page-404',
+      template: `
+            <div class="container">
+                  Page Not Found
+            </div>`,
+      styles: ['']
+})
+
+export class PageNotFound {
+
+}
